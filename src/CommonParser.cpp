c840e04518937c5e8c5f5d49a091b760475b89e9
de12161 <162108835+de12161@users.noreply.github.com>
2024-12-03 12:18:42 +0300

    Update CommonParser.cpp

--- src/CommonParser.cpp
+++ src/CommonParser.cpp
@@ -2,11 +2,15 @@
 #include <cstdlib>
 #include "include/parsers.hpp"
 
-double CommonParser::parse(const std::list<Token>& tokens)
+void CommonParser::init(const std::list<Token>& tokens)
 {
   it = tokens.begin();
   end = tokens.end();
+}
 
+double CommonParser::parse(const std::list<Token>& tokens)
+{
+  init(tokens);
   return parseExpr()->eval();
 }
 
@@ -17,24 +21,70 @@ Pointer<IExpr> CommonParser::parseExpr()
 
 Pointer<IExpr> CommonParser::parseAddExpr()
 {
-  Pointer<IExpr> lhs = parseNumericLiteral();
+  Pointer<IExpr> lhs = parseMulExpr();
   Pointer<IExpr> rhs;
-
 
   switch (it->getType())
   {
     case Add:
       ++it;
-      rhs = parseExpr();
+      rhs = parseAddExpr();
       return new AddExpr(lhs, rhs);
 
     case Sub:
       ++it;
-      rhs = parseExpr();
+      rhs = parseAddExpr();
       return new SubExpr(lhs, rhs);
 
     default:
       return lhs;
+  }
+}
+
+Pointer<IExpr> CommonParser::parseMulExpr()
+{
+  Pointer<IExpr> lhs = parseUnaryExpr();
+  Pointer<IExpr> rhs;
+  
+  switch (it->getType())
+  {
+    case Mul:
+      ++it;
+      rhs = parseMulExpr();
+      return new MulExpr(lhs, rhs);
+
+    case Div:
+      ++it;
+      rhs = parseMulExpr();
+      return new DivExpr(lhs, rhs);
+
+    default:
+      return lhs;
+  }
+}
+
+Pointer<IExpr> CommonParser::parseUnaryExpr()
+{
+  switch (it->getType())
+  {
+  	case Exp:
+  	  ++it;
+  	  return new ExpExpr(parseUnaryExpr());
+  	  
+  	case Log:
+  	  ++it;
+  	  return new LogExpr(parseUnaryExpr());
+  	
+    case Sqr:
+      ++it;
+      return new SqrExpr(parseUnaryExpr());
+      
+    case Sqrt:
+      ++it;
+      return new SqrtExpr(parseUnaryExpr());
+    
+    default:
+      return parseNumericLiteral();
   }
 }
 
