02cd56042a1b747a71d298c2e3a3b2e39bf591e4
dec161 <162108835+dec161@users.noreply.github.com>
2024-12-09 14:28:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,55 +2,46 @@
 #include "include/parsers.hpp"
 #include <iostream>
 
-void testOutput(const Pointer<IParser>& parser, const Lexer& lexer, const std::string& src, double expected)
+void printUsage()
 {
-  std::list<Token> tokens = lexer.tokenize(src);
-  double actual = parser->parse(tokens);
-  
-  std::cout << "Input: " << src << '\n'
-            << "Result: " << actual << '\n';
-  
-  if (actual != expected)
-  {
-    std::cout << "Test failed" << '\n'
-	          << "Expected result: " << expected << '\n';
-  }
+  std::cout << "Usage:\n"
+            << "calc MODE EXPRESSION\n";
 }
 
-int main()
+int main(int argc, char** argv)
 {
+  if (argc < 2) 
+  {
+    printUsage();
+    return 1;
+  }
+	
   Lexer lexer;
-  Pointer<IParser> parser = new CommonParser;
-  std::list<Token> tokens;
-
-  testOutput(parser, lexer, "5", 5);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 * 2", 10);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 + 3", 8);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 + 3 * 2", 11);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 * 3 + 2", 17);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 + 4 + 3 + 2", 14);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 * 4 * 3 * 2", 120);
-  std::cout << '\n';
-  testOutput(parser, lexer, "sqr 5", 25);
-  std::cout << '\n';
-  testOutput(parser, lexer, "5 * sqrt sqr 2", 10);
-  std::cout << '\n';
-  testOutput(parser, lexer, "(1)", 1);
-  std::cout << '\n';
-  testOutput(parser, lexer, "(5 + 3) * 2", 16);
-  std::cout << '\n';
-  testOutput(parser, lexer, "2 * (5 + 3)", 16);
-  std::cout << '\n';
-  testOutput(parser, lexer, "(1 + (2 + (3 * (2 + 2))))", 15);
-  std::cout << '\n';
-  testOutput(parser, lexer, "(1 + sqr (2 + (sqrt 4 + 3 * (2 + 2))))", 257);
-  std::cout << '\n';
-
+  Pointer<IParser> parser;
+		
+  if (argv[1] == std::string("-r"))
+  {
+    parser = new ReverseParser;
+  }
+  else if (argv[1] == std::string("-c"))
+  {
+    parser = new CommonParser;
+  }
+  else
+  {
+    printUsage();
+    return 1;
+  }
+	
+  std::string input;
+	
+  for (int i = 2; i < argc; i++) 
+  {
+    input += argv[i];
+    input += ' ';
+  }
+	
+  std::cout << parser->parse(lexer.tokenize(input)) << '\n';
+	
   return 0;
 }
